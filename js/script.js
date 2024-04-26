3e75169760ce5b5d57c577239416e98c32a26289
neohyntw <neohyntw@gmail.com>
2024-04-26 10:20:46 +0800

    commit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(window).scroll(function() {
     // Get the scroll position
-    var scrollTop = $(window).scrollTop();
+    let scrollTop = $(window).scrollTop();
     
     // Show the icon when scrolled down by a certain amount
     if (scrollTop > 200) { // Adjust this threshold to your preference
